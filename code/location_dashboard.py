84d2a4ae48d3d60280567152fd5e5450372fbda7
grusso01 <grusso01@syr.edu>
2025-04-23 23:03:54 -0400

    the second commit message

--- code/location_dashboard.py
+++ code/location_dashboard.py
@@ -6,3 +6,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 st.set_page_config(layout="wide")
+
+data = pd.read_csv('./cache/tickets_in_top_locations.csv')
+st.title('Top Locations for Parking Tickets in Syracuse')
+st.caption('This shows the parking tickets that were issued in the top locations with $1,000 or more in total violation amounts.')
+
+unique_locations = data['location'].unique()
+selected_location = st.selectbox('Select a location:', unique_locations)
+
+if selected_location:
+    subset = data[data['location'] == selected_location]
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric("Total tickets issued", subset.shape[0])
+        fig1, ax1 = plt.subplots()
+        ax1.set_title('Tickets Issued by Hour of Day')
+        sns.barplot(data=subset, x="hourofday", y="count", estimator="sum", hue="hourofday", ax=ax1)
+        st.pyplot(fig1)
+
+    with col2:
+        st.metric("Total amount", f"$ {subset['amount'].sum()}")
+        fig2, ax2 = plt.subplots()
+        ax2.set_title('Tickets Issued by Day of Week')
+        sns.barplot(data=subset, x="dayofweek", y="count", estimator="sum", hue="dayofweek", ax=ax2)
+        st.pyplot(fig2)
+
+    st.map(subset[['lat', 'lon']])
--- code/map_dashboard.py
+++ code/map_dashboard.py
@@ -12,3 +12,18 @@ CUSE = (43.0481, -76.1474)  # center of map
 ZOOM = 14                   # zoom level
 VMIN = 1000                 # min value for color scale
 VMAX = 5000                 # max value for color scale
+
+locations_df = pd.read_csv('./cache/top_locations_mappable.csv')
+geo_df = gpd.GeoDataFrame(locations_df, geometry=gpd.points_from_xy(locations_df.lon, locations_df.lat))
+
+st.title('Top Locations for Parking Tickets in Syracuse')
+st.caption('This shows the parking tickets that were issued in the top locations with $1,000 or more in total violation amounts.')
+
+map = folium.Map(location=CUSE, zoom_start=ZOOM)
+cuse_map = geo_df.explore(geo_df['amount'], m=map, 
+                       cmap="magma",vmin=VMIN, vmax=VMAX, 
+                       legend=True, legend_name='Amount',
+                       marker_type = "circle",
+                       marker_kwds = {"radius": 10, "fill": True},
+)
+sf.folium_static(cuse_map, width=800, height=600)
